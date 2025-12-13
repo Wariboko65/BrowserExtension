1007582610bd824edf4e9e74ccb5de80d1a088ab
wariboko65 <oamienjoh20034@gmail.com>
2025-12-14 00:12:11 +0100

    scrollbar jump fix

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -7,10 +7,16 @@ export default function Modal({ id, children, modalId, closeModal, removeItem })
     const modalOpen = modalId === id;
     useEffect(() => {
         if (modalOpen) {
+            const getScrollbarWidth = () => {window.innerWidth - document.documentElement.clientWidth; }
+            const scrollbarWidth = getScrollbarWidth();
             document.body.style.overflow = "hidden";
-        }
-        return () => {
+            document.body.style.paddingRight = `${scrollbarWidth}px`;
+                
+
+        }  return () => {
             document.body.style.overflow = "auto";
+            document.body.style.paddingRight = "";
+
         }
     }, [modalOpen]);
    
